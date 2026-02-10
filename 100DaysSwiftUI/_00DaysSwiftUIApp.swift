6be2acbb6864cd3c90273f4a5ce9eccb27d92f74
Dev-Zinab <St.biology1991@gmail.com>
2026-02-10 21:55:14 +0300

    Initial Commit

---
+++ 100DaysSwiftUI/_00DaysSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  _00DaysSwiftUIApp.swift
+//  100DaysSwiftUI
+//
+//  Created by Zinab Zooba on 10/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct _00DaysSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
